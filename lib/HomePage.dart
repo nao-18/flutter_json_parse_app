87ab0c6cdcd4071a6a28a1a4bde374889087e822
nao-18 <kyahyo17@yahoo.co.jp>
2020-05-29 09:52:27 +0900

    finished list view

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 import 'Model/Data.dart';
+import 'DetailPage.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -12,11 +13,12 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<MaterialColor> _color = [
     Colors.deepOrange,
+    Colors.amber,
     Colors.red,
     Colors.green,
     Colors.purple,
     Colors.brown,
-   ];
+  ];
 
   Future<List<Data>> getAllData() async {
     var api = "https://jsonplaceholder.typicode.com/photos";
@@ -132,7 +134,7 @@ class _HomeState extends State<Home> {
 ////                  return Container(
 ////                    child: Text('OK'),
 ////                  );
-                  print("length: ${snapshot.data.length}");
+//                  print("length: ${snapshot.data.length}");
                   return ListView.builder(
                     itemCount: snapshot.data.length,
                     scrollDirection: Axis.horizontal,
@@ -190,6 +192,79 @@ class _HomeState extends State<Home> {
                 }
               },
             ),
+          ),
+          new SizedBox(height: 7.0),
+          new Container(
+            margin: EdgeInsets.all(10.0),
+            height: MediaQuery.of(context).size.height,
+            child: FutureBuilder<Object>(
+                future: getAllData(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.data == null) {
+                    return Center(
+                      child: new Text("Loading Data...."),
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        MaterialColor mColor = _color[index % _color.length];
+                        return Card(
+                          elevation: 7.0,
+                          child: Container(
+                            height: 80.0,
+                            child: new Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                new Expanded(
+                                  flex: 1,
+                                  child: new Image.network(
+                                    snapshot.data[index].thumbnailUrl,
+                                    height: 100.0,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                new SizedBox(
+                                  width: 6.0,
+                                ),
+                                new Expanded(
+                                  flex: 2,
+                                  child: InkWell(
+                                    child: new Text(
+                                      snapshot.data[index].title,
+                                      maxLines: 2,
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                      ),
+                                    ),
+                                    onTap: () {
+                                      Navigator.of(context).push(
+                                        new MaterialPageRoute(
+                                          builder: (BuildContext context) => Detail(data: snapshot.data[index]),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                                new Expanded(
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: CircleAvatar(
+                                      child: new Text(
+                                          snapshot.data[index].id.toString()),
+                                      backgroundColor: mColor,
+                                      foregroundColor: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                }),
           ),
         ],
       ),
